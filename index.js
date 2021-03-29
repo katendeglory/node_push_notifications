a3a8a744539c45b4e8b4539d02cbe789b707812f
Glory Katende <katendeglory@gmail.com>
2021-03-30 00:57:05 +0200

    Yet another commit

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
 const express = require("express");
 const webpush = require("web-push");
-const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 
-// Set static path
-app.use(express.static(path.join(__dirname, "client")));
+app.use(require('cors')());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
+/* ----------------------- 👇 PUSH NOTIFICATIONS AREAS 👇 ----------------------- */
 
-const publicVapidKey =
-  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
-const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
+// Generated from https://vapidkeys.com/
+const publicVapidKey = "BJR38iSDBijTfcWD_jqvoSFxu9_w6UxRPrJKXj1oiN8c4qPH-tv6qPcGAp-ZJAzJaMJ8kMKaKJHGWc6LrIAHHAU";
+const privateVapidKey = "f_C3ehDj81b7qLU-GO8-9SrlJLG8P3lloPlJayLDDhU";
 
 webpush.setVapidDetails(
-  "mailto:test@test.com",
+  "mailto:test@example.com",
   publicVapidKey,
   privateVapidKey
 );
 
-// Subscribe Route
-app.post("/subscribe", (req, res) => {
-  // Get pushSubscription object
+app.post("/notify-sub", (req, res) => {
   const subscription = req.body;
-
-  // Send 201 - resource created
   res.status(201).json({});
-
-  // Create payload
-  const payload = JSON.stringify({ title: "Push Test" });
-
-  // Pass object into sendNotification
+  const payload = JSON.stringify({ title: "Hey there, this is a cool notification!" });
   webpush
     .sendNotification(subscription, payload)
     .catch(err => console.error(err));
 });
 
-const port = 5000;
+app.post("/add-sub", (req, res) => {
+  // Save it to the database for scheduled notifications
+});
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Chron Job notify a whole bunch of people here 👇 ...
+// ...
+
+//Test
+setTimeout(() => {
+  console.log("Ready...");
+  const payload = JSON.stringify({ title: "Hey there, this is a cool notification!" });
+  let subscription = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/dY1AHZ2frzo:APA91bFNa2AgTSjBx_cMUNq2V6E_DHuFGRbIHMQB6mISP0kmXSonxoOusKUFbkDRk_655QzN0GgjWQaSRWUcuQNe15VTV7UOa5KHz5gJ0CTt99k_0RBGMQoEXWbh78L7wrzmTotXEHlW",
+    "expirationTime": null,
+    "keys": {
+      "p256dh": "BJDIHNVSmGHO8S8y-qn3elqs_RQvjZK_3L3W9DupL8vsQaqQlkQbM8kWGMnwX9-UeBXxW1ebvSYh4ZijRgS_BSk",
+      "auth": "A-gXvVz32RVYOYAEB_4UkA"
+    }
+  }
+  webpush.sendNotification(subscription, payload).then(res => console.log("Done!!!")).catch(err => console.log(err));
+}, 10000);
+//Test
+
+/* ----------------------- 👆 PUSH NOTIFICATIONS AREAS 👆 ----------------------- */
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
